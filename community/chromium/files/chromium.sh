f2870db34468ea16a66be05d8fe1f665490ff86e
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2022-08-21 14:50:06 +0530

    chromium: 104.0.5112.102 1

--- community/chromium/files/chromium.sh
+++ community/chromium/files/chromium.sh
@@ -2,7 +2,6 @@
 
 # shellcheck disable=2086
 exec /usr/lib/chromium/chromium \
-    --ozone-platform=wayland \
-    --enable-features=UseOzonePlatform \
     --enable-gpu-rasterization \
+    --ozone-platform-hint=auto \
     $CHROME_FLAGS "$@"
